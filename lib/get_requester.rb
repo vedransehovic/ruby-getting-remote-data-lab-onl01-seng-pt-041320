dfd0e463ce01d46217460d33d147ae3687e86c89
vedransehovic <vedran.sehovic@gmail.com>
2020-05-15 18:08:58 -0400

    Done.

---
+++ lib/get_requester.rb
@@ -0,0 +1,23 @@
+require 'pry'
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        programs = JSON.parse(self.get_response_body)
+    end
+end
+ 
